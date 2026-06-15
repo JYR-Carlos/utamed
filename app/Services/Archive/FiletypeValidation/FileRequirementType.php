a9ad69f2887edfbf146916eb958e4d52970a99a7
dyri0n <tomassilva0360@gmail.com>
2026-06-15 11:34:06 -0400

    wip(fix): separacion archivos filerequirement

---
+++ app/Services/Archive/FiletypeValidation/FileRequirementType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Rules\Builders;
+
+enum FileRequirementType: string
+{
+  case VIDEO = 'video';
+  case IMAGE = 'image';
+  case COMPRESSED = 'compressed';
+  case WORD_DOCUMENT = 'word_document';
+  case PRESENTATION = 'presentation';
+  case SPREADSHEET = 'spreadsheet';
+  case PDF = 'pdf';
+  case LINK = 'link';
+  case MEDIA = 'media';
+  case RAW_ART = 'raw_art';
+  case DOCUMENT = 'document';
+
+  /**
+   * Devuelve el nombre legible para los mensajes de error.
+   */
+  public function label(): string
+  {
+    return match ($this) {
+      self::VIDEO => 'videos',
+      self::IMAGE => 'imágenes',
+      self::COMPRESSED => 'archivos comprimidos',
+      self::WORD_DOCUMENT => 'documentos de Word',
+      self::PRESENTATION => 'presentaciones',
+      self::SPREADSHEET => 'hojas de cálculo',
+      self::PDF => 'PDFs',
+      self::LINK => 'enlaces',
+      self::MEDIA => 'archivos multimedia',
+      self::RAW_ART => 'archivos de arte crudo (RAW/PSD/AI)',
+      self::DOCUMENT => 'documentos genéricos',
+    };
+  }
+}
